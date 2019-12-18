e33fb8933815ac7b02b315744c71f901cf1eda13
Mrunalini Prasad <minniepk11@gmail.com>
2019-12-18 19:17:38 +0530

    removed extra code

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -11,23 +11,14 @@ import { Order } from '../models/order.model';
 })
 export class OrdersComponent implements OnInit {
 
-  // colHeaderOrders: any[];
+  
   orders: any = [];
   errormsgs: Message[] = [];
   
   
   constructor(private orderResultsService: OrderResultsService) {
-    // this.colHeaderOrders = [
-    //   {field: 'recipient',  subfield: 'name', header:'Customer Name', width:'25%'},
-    //   {field: 'email', subfield: 'email', header:'Customer Email', width: '25%'},
-    //   {field: 'items', subfield: 'total_price', header:'Total Price', width: '10%'},
-    //   {field: 'created_at', header:'Date Time', width:'25%'},
-    //   {field: 'items', subfield:'id', header:'Item Id', width:'15%'},
-    //   {field: 'items', header:'name', width: '25%'},
-    //   {field: 'delivery', subfield: 'courier', header:'Courier', width:'7%'},
-    //   {field: 'delivery', subfield: 'method', header:'Method', width:'10%'}
-    // ]
-   }
+    
+  }
 
   ngOnInit() {
     this.loadData();
@@ -35,7 +26,7 @@ export class OrdersComponent implements OnInit {
   }
 
   loadData(){
-      this.orderResultsService.getOrderResult().subscribe((data) =>   {        // this.orders = Object.keys(data).map(key => data[key])
+      this.orderResultsService.getOrderResult().subscribe((data) =>   {        
           this.orders = data;
           console.log("inside component", this.orders);   
       }), error => {
@@ -43,12 +34,5 @@ export class OrdersComponent implements OnInit {
       }
   }
 
-  // populateData(){
-  //     this.recepientName = this.orders['name'];
-  //     this.totalPrice = this.orders['total_price'];
-  //     this.createdDate = this.orders['created_at'];
-  //     this.items = this.orders['items'];
-  //     this.deliveryDetails = this.orders['delivery'];
-  // }
-
+ 
 }
